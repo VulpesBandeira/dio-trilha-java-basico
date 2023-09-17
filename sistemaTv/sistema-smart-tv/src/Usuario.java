3d02ab61d6951f997ab7be678e5d4d7af125bd86
VulpesBandeira <tatasha_oliver@hotmail.com>
2023-09-17 13:52:19 -0300

    projeto-smartTV

--- sistemaTv/sistema-smart-tv/src/Usuario.java
+++
@@ -1,8 +0,0 @@
-public class Usuario {
-    public static void main(String[] args) throws Exception {
-        
-        SmartTv smartTv = new SmartTv();
-
-        System.out.println("TV Ligada?" + smartTv.ligada);
-    }
-}
